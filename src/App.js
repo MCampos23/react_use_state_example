d57d3402d951acee23b8552d4bcaeef1fc3dd3f1
MCampos23 <camposvmiguelangel@gmail.com>
2024-09-28 01:12:15 +0200

    first comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState  } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Ejemplo más elaborado de uso del hook use_state
+  const [items, setItems] = useState([]);
+const addItem = () => setItems([...items, { id: items.length, value: Math.random() * 100 }]);
+return (<div><button onClick={addItem}>Add Item</button><ul>{items.map(item => <li key={item.id}>{item.value}</li>)}</ul></div>);
+
 }
 
 export default App;
